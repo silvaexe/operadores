566deb5d974f2214add6e24e3848ebbaa0d70cbc
Matheus da silva <matheussilvainter1234@gmail.com>
2024-09-30 20:51:00 -0300

    calculos

---
+++ exercicio1.js
@@ -0,0 +1,25 @@
+let primeiroValor = 3
+let segundoValor = 4
+console.log (primeiroValor + segundoValor) //7
+
+ primeiroValor = 3
+ segundoValor = 5
+ terceiroValor = 2
+console.log(primeiroValor * segundoValor / terceiroValor) //7.5
+
+primeiroValor = 5
+segundoValor = 4
+terceiroValor = -1
+console.log( (segundoValor - primeiroValor ) * terceiroValor)
+
+primeiroValor = 234
+segundoValor = 5
+console.log(primeiroValor % segundoValor) 
+
+
+
+
+
+
+
+
