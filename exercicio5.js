6e433db95660e05b074ad07f4fd89baaa84df3fd
Matheus da silva <matheussilvainter1234@gmail.com>
2024-10-01 20:10:44 -0300

    exercicio 5

---
+++ exercicio5.js
@@ -0,0 +1,11 @@
+const nome = "Matheus"
+const idade = 18
+
+console.log("Nome: ",  nome)
+console.log("idade:", idade)
+
+console.log("maior de idade?", idade >= 18)
+console.log("idade em 2050:",2050 - 2024 + idade)
+
+
+
