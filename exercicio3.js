1a130b4c6e931b2e0d968c806b5cb0fbce8693df
Matheus da silva <matheussilvainter1234@gmail.com>
2024-09-30 21:48:30 -0300

    exercicio 3

---
+++ exercicio3.js
@@ -0,0 +1,9 @@
+const a = true
+const b = false
+const c = true
+
+console.log( a  && b ,"false" )
+console.log( b  && c ,"false" )
+console.log( a  && c , "true" )
+console.log( a  && b && c ,"false" )
+
